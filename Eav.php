3b06e941a34252fbc964dfd7837f590dc2fb86d5
Dave Baker <dbaker@iweb.co.uk>
2016-05-12 11:16:17 +0100

    Added the fieldname to the method's return

--- Eav.php
+++ Eav.php
@@ -1,6 +1,6 @@
 <?php
 class Llapgoch_Core_Helper_Eav extends Mage_Core_Helper_Abstract{
-    protected $_aliasIndex = 0;
+	protected $_aliasIndex = 0;
 
     public function joinEAV($collection, $mainTableForeignKey, $eavType, $attrCode){
         $this->_aliasIndex++;
@@ -12,17 +12,18 @@ class Llapgoch_Core_Helper_Eav extends Mage_Core_Helper_Abstract{
         $attribute = Mage::getModel("eav/config")->getAttribute($eavType, $attrCode);
 
         $attr =  Mage::getModel('eav/entity_attribute')->loadByCode($eavType, $attrCode);
-        
+
         $alias = 'table_' . $this->_aliasIndex;
+        $field = $attrCode; // This will either be the original attribute code or 'value'
 
         if ($attribute->getBackendType() != 'static'){
-            $field = $alias.'.value';
+            $field = 'value';
             $table = $entityTable. '_'.$attribute->getBackendType();
 
             $collection->getSelect()
                 ->joinLeft(array($alias => $table),
                     'main_table.'.$mainTableForeignKey.' = '.$alias.'.entity_id and '.$alias.'.attribute_id = '. $attr->getId(),
-                    array($attribute->getAttributeCode() => $field)
+                    array($attribute->getAttributeCode() => $alias . "." . $field)
                 );
         }else{
             $collection->getSelect()
@@ -32,7 +33,11 @@ class Llapgoch_Core_Helper_Eav extends Mage_Core_Helper_Abstract{
                 );
         }
 
-        // Return the alias so we can use the table in future queries
-        return $alias;
+        // Return the table alias and field name (either $attrCode or value) so we can use the table in future queries
+        return array(
+            "table" => $alias,
+            "field" => $field
+        );
+
     }
 }
